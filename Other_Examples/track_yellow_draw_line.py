c1b1a1673d70db5dd7b8b9a8d6d7cd85f83fb64c
abid <abidrahman2@gmail.com>
2011-03-29 19:09:09 +0530

    Tracking a yellow object and draw line

--- Other_Examples/track_yellow_draw_line.py
+++ Other_Examples/track_yellow_draw_line.py
@@ -1,4 +1,11 @@
-''' this program just track a yellow object in front of camera and draws a yellow line according to movement of object'''
+'''	 track_yellow_draw_line.py
+
+This program just track a yellow object in front of camera and draws a yellow line according to movement of the object.
+
+Written by Abid.K	--mail me at abidrahman2@gmail.com	'''
+
+################################################################################################
+
 import cv
 posx=0
 posy=0
@@ -6,9 +13,9 @@ posy=0
 def getthresholdedimg(im):
 	'''this function take RGB image.Then convert it into HSV for easy colour detection and threshold it with yellow part as white and all other regions as black.Then return that image'''
 	imghsv=cv.CreateImage(cv.GetSize(im),8,3)
-	cv.CvtColor(im,imghsv,cv.CV_BGR2HSV)
+	cv.CvtColor(im,imghsv,cv.CV_BGR2HSV)				# Convert image from RGB to HSV
 	imgthreshold=cv.CreateImage(cv.GetSize(im),8,1)
-	cv.InRangeS(imghsv,cv.Scalar(20,100,100),cv.Scalar(30,255,255),imgthreshold)
+	cv.InRangeS(imghsv,cv.Scalar(20,100,100),cv.Scalar(30,255,255),imgthreshold)	# Select a range of yellow color
 	return imgthreshold
 	
 def getpositions(im):
@@ -42,9 +49,9 @@ cv.NamedWindow("output")
 while(1):
 	frame=cv.QueryFrame(capture)
 	cv.Flip(frame,frame,1)
-	imdraw=cv.CreateImage(cv.GetSize(frame),8,3)	#we make all drawings on imdraw.
-	imgyellowthresh=getthresholdedimg(frame)	#we get coordinates from imgyellowthresh
-	cv.Erode(imgyellowthresh,imgyellowthresh,None,1)#eroding removes small noises
+	imdraw=cv.CreateImage(cv.GetSize(frame),8,3)	# we make all drawings on imdraw.
+	imgyellowthresh=getthresholdedimg(frame)	# we get coordinates from imgyellowthresh
+	cv.Erode(imgyellowthresh,imgyellowthresh,None,1)# eroding removes small noises
 	(leftmost,rightmost,topmost,bottommost)=getpositions(imgyellowthresh)
 	if (leftmost-rightmost!=0) or (topmost-bottommost!=0):
 		lastx=posx
@@ -55,8 +62,10 @@ while(1):
 			cv.Line(imdraw,(posx,posy),(lastx,lasty),(0,255,255))
 			cv.Circle(imdraw,(posx,posy),5,(0,255,255),-1)
 
-	cv.Add(test,imdraw,test)		# adding imdraw on test keeps all lines there on the test frame. If not, we don't get full drawing, instead we get only that fraction of line at the moment.
+	cv.Add(test,imdraw,test)			# adding imdraw on test keeps all lines there on the test frame. If not, we don't get full drawing, instead we get only that fraction of line at the moment.
 	cv.ShowImage("output",test)
-	if cv.WaitKey(33)==1048603:		#exit if Esc key is pressed
+	if cv.WaitKey(33)==1048603:			# exit if Esc key is pressed
 		break
-cv.DestroyWindow("output")			# releasing window
+cv.DestroyWindow("output")				# releasing window
+#######################################################################################################
+## Please try mouse_callback.py and then read pick_and_track.py
