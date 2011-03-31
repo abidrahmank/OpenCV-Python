dc070360583859bf974891a64770a5380f3e1d2f
abid <abidrahman2@gmail.com>
2011-04-01 03:41:40 +0530

    Camera Calibration

---
+++ Learning_OpenCV-Python/example_11_1.py
@@ -0,0 +1,13 @@
+''' 
+
+Referred to example 11.1 in the book "Learning OpenCV: Computer Vision with the OpenCV Library"
+
+Example 11.1 - Reading a chessboard’s width and height, reading and collecting the requested number of views, and calibrating the camera
+
+NB: A little modified version of this example is given in Other_Examples/camera_calibration.py. Please check it. 
+
+Brought to you by Abid.K		--mail me at abidrahman2@gmail.com
+
+'''
+
+
